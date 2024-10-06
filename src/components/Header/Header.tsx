ad8e8a13fbaedb7c54c968d8c4117b7c345cf693
Simfart <davidlemondzava265@gmail.com>
2024-10-06 14:59:46 +0300

    created header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import './Header.scss';
+import logo from '../../assets/pngwing.com (4).png';
+
+export default function Header(){
+    return <header className='header'>
+        <img src={logo} width='200' height='200'  className="logo"/>
+        <nav className='header-nav'>
+            <Link className='header-nav__item link-home' to='/'>Home</Link>
+            <Link className='header-nav__item link-shop' to='/'>Shop</Link>
+        </nav>
+    </header>
+}
