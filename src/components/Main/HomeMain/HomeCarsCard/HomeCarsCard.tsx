246ae8c8662c1356fe1bc6cb92215b2ab43d0af9
Simfart <davidlemondzava265@gmail.com>
2024-10-15 21:41:41 +0300

    start card

---
+++ src/components/Main/HomeMain/HomeCarsCard/HomeCarsCard.tsx
@@ -0,0 +1,11 @@
+import './Card.scss';
+
+export default function HomeCarsCard(){
+    return <section className='cards'>
+        <div className='cards-container'>
+            <div className="cards-container__item"></div>
+            <div className="cards-container__item"></div>
+            <div className="cards-container__item"></div>
+        </div>
+    </section>
+}
--- src/components/Main/HomeMain/Main.tsx
+++ src/components/Main/HomeMain/Main.tsx
@@ -1,8 +1,10 @@
+import HomeCarsCard from './HomeCarsCard/HomeCarsCard'
 import HomeCarsSlider from './HomeCarsSlider/HomeCarsSlider'
 import './Main.scss'
 
 export default function HomeMain(){
     return <main className='main'>
         <HomeCarsSlider></HomeCarsSlider>
+        <HomeCarsCard></HomeCarsCard>
     </main>
 }
