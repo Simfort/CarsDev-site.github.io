468bbcb627e09ffd38c12452e1eba26a4471f2cc
Simfort <lemondzavadavid265@gmail.com>
2024-10-30 14:33:40 +0300

    Canceled home-main

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
+import './Footer.scss'
+
 export default function Footer(){
-    return <footer style={{background:'black', height:`100px`,color:"white",display:"flex",justifyContent:'space-around',alignItems:'center'}} className="footer">
-        <p style={{fontFamily:"sans-serif"}}>Thank you about visit on my site!</p>
-        <p style={{fontFamily:"sans-serif"}}>mersedez@gmail.gey</p>
+    return <footer className="footer">
+        <p >Thank you about visit on my site!</p>
+        <p >mersedez@gmail.gey</p>
     </footer>
 }
